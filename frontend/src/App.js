b56a53addef9344cb6a30bfc3897c306e33b811f
Yatindra Panchal <yatindrap6@gmail.com>
2025-07-27 13:46:01 +0530

    Re-added frontend folder as normal directory (removed submodule issue)

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frontend React App</h1>
+      <p>Fetching from microservices coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
+
